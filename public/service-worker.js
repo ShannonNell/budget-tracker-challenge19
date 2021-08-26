fd64eadd97555497910bddb039d5bbf8da2c5dbf
Shannon <shannon.nell92@gmail.com>
2021-08-26 14:08:06 -0400

    SW activated; Fetch requests intercepted

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,6 +4,7 @@ const CACHE_NAME = APP_PREFIX + VERSION;
 
 // images not necessary to cache? Or ok because they're so small and this is a small app?
 const FILES_TO_CACHE = [
+    "/",
     "./index.html",
     "./css/styles.css",
     "./js/index.js",
@@ -18,11 +19,49 @@ const FILES_TO_CACHE = [
     "./icons/icon-512x512.png",
 ];
 
+// install service worker
 self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
             console.log('installing cache : ' + CACHE_NAME)
             return cache.addAll(FILES_TO_CACHE)
+        })
+    )
+});
+
+// activate service worker
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            let cacheKeeplist = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            });
+            cacheKeeplist.push(CACHE_NAME);
+
+            return Promise.all(keyList.map(function (key, i) {
+                if (cacheKeeplist.indexOf(key) === -1) {
+                    console.log('deleting cache : ' + keyList[i]);
+                    return caches.delete(keyList[i]);
+                }
+            })
+          );
+        })
+    );
+});
+
+
+// retrieve info from cache
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) {
+                console.log('Responding with cache : ' + e.request.url);
+                return request;
+            } else {
+                console.log('File is not cached, fetching : ' + e.request.url);
+                return fetch(e.request);
+            }
         })
     )
 });
